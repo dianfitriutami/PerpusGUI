00c29d7d29133a1d369b04f5989871d48e5eeffa
dianfitriutami <111755680+dianfitriutami@users.noreply.github.com>
2023-10-31 12:02:11 +0700

    f

--- PerpusGUI/src/perpusgui/PerpusGUI.java
+++ PerpusGUI/src/perpusgui/PerpusGUI.java
@@ -15,6 +15,7 @@ public class PerpusGUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("widioawoidjmoawijdoia");
     }
     
 }
